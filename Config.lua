97b4bfba72a5827481e8877600fc28436dd41d93
johan626 <j6749520@gmail.com>
2025-10-29 12:20:50 +0700

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -6,7 +6,13 @@ local Config = {
 	SNAP_INTERVAL = 0.1,
 	TOTAL_TIME = 30,
 	TRACK_HEIGHT = 20,
-	LANE_PADDING = 5
+	LANE_PADDING = 5,
+
+	TrackColors = {
+		Light = Color3.fromRGB(200, 180, 80),
+		Sound = Color3.fromRGB(80, 180, 200),
+		Particle = Color3.fromRGB(180, 80, 200)
+	}
 }
 
 return Config
