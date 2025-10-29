7d4cf800dac805ea6ac22a33c0a9a4e5db21dfd7
johan626 <j6749520@gmail.com>
2025-10-30 06:44:45 +0700

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -11,7 +11,11 @@ local Config = {
 	TrackColors = {
 		Light = Color3.fromRGB(200, 180, 80),
 		Sound = Color3.fromRGB(80, 180, 200),
-		Particle = Color3.fromRGB(180, 80, 200)
+		Particle = Color3.fromRGB(180, 80, 200),
+		SpotLight = Color3.fromRGB(255, 255, 150),
+		SurfaceLight = Color3.fromRGB(255, 200, 150),
+		Beam = Color3.fromRGB(150, 255, 255),
+		Trail = Color3.fromRGB(255, 150, 255)
 	}
 }
 
